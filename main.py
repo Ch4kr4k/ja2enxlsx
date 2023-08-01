5b8fdc0ed214594a6d488dcb36f237947d0a6826
Ch4kr4k <marakmuna9@gmail.com>
2023-08-02 01:42:13 +0530

    bare bones and concept

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from googletrans import Translator, constants
+from pprint import pprint
+
+def main():
+    jp = Translator()
+    #ja = str(input("input japanese romanji :"))
+    #eng = jp.translate(ja, dest="en", src="ja" )
+    #print(f"translated: {eng.text}")
+if __name__ == '__main__':
+    main()
